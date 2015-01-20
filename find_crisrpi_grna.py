6255a1774a800722fad1a665d4173481f01b3189
Jacob Corn <jcorn@berkeley.edu>
2015-01-19 17:03:31 -0800

    find_crispri_grna.py: Clean up unused function defs

--- find_crisrpi_grna.py
+++ find_crisrpi_grna.py
@@ -11,17 +11,6 @@ import sys, subprocess, os, tempfile, copy
 
 from Crispr import *
 
-def parse_target_header( sequence_record ):
-#reads fasta headers of the format
-#>id, start base,premutation sequence,end base,postmutation sequence
-#for example
-#>gene foo bar,100,ATG,103,TAA
-	id, start, seq, end, mutation = sequence_record.id.split(",")
-	start = int(start)
-	end = int(end)
-	assert( end-start+1 ) == len(seq)
-	return id, start, end, seq, mutation
-
 def sg_to_seqfeature( target, sg ):
 	location = target.seq.find( sg.protospacer.back_transcribe() )
 	if location == -1:
@@ -37,53 +26,6 @@ def sg_to_seqfeature( target, sg ):
 	quals = { "protospacer": sg.protospacer, "score": sg.score }
 	feature = SeqFeature( FeatureLocation( location, location+len(sg.protospacer)), strand=strand, type="sgRNA", id=id, qualifiers=quals )
 	return feature
-
-def hr_to_seqfeature( target, hr ):
-	location = target.seq.find( hr.target_seq )
-	if location == -1:
-		print "Could not find hr template in target!"
-		return None
-	strand = None
-	id = hr.seq
-	quals = {"sequence": hr.seq }
-	feature = SeqFeature( FeatureLocation( location, location+len(hr.seq)), strand=strand, type="hrtemplate", id=id, qualifiers=quals)
-	return feature
-
-def plot_sgrnas( target, target_basen, offset, sites ):
-	gd_diagram = GenomeDiagram.Diagram( )
-	gd_track_for_features = gd_diagram.new_track(1, name="sgRNAs")
-	gd_feature_set = gd_track_for_features.new_set()
-	for site in sites:
-		record = site[0].split('_')
-		target_sequence = record[0]
-		target_id = record[1]
-		strand = record[2]
-		sgrna = ''
-		if strand == 'sense':
-			sgrna = Seq(target_sequence[:-2], generic_dna)
-		elif strand== 'as':
-			sgrna = Seq(target_sequence[2:], generic_dna)
-			sgrna = sgrna.reverse_complement()
-		else:
-			print "Strand not recognized!"
-		start = target.seq.find( target_sequence )
-		if strand == 'sense':
-			feature = SeqFeature(FeatureLocation(start,start+23),strand=+1)
-			gd_feature_set.add_feature(feature, color="cyan", label=True, name=str(sgrna), label_angle=0, sigil="ARROW")
-		else:
-			feature = SeqFeature(FeatureLocation(start,start+23),strand=-1)
-			gd_feature_set.add_feature(feature, color="blue", label=True, name=str(sgrna), label_angle=0, sigil="ARROW")
-		print target_sequence, target_id, strand, sgrna
-
-	feature = SeqFeature(FeatureLocation(target_basestart, target_basestart+1))
-	gd_feature_set.add_feature(feature, color="red", label=False)
-	
-	feature = SeqFeature(FeatureLocation(0, len(target.seq)), strand=+1)
-	gd_feature_set.add_feature(feature, color="blue", label=True, name=str(target), label_angle=0, hide=False, label_size=25)
-	
-	gd_diagram.draw(format="linear", pagesize='LETTER', fragments=1, start=target_basen-offset, end=target_basen+offset)
-	gd_diagram.write(target_id+".pdf", "PDF")
-
 
 # start of main script
 target_fname = sys.argv[1]
